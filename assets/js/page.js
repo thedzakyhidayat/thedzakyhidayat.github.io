643497b42f5e9c8305baab4ca196fac8613d950a
toolazyforname <xxxzack24@gmail.com>
2021-02-01 19:07:26 +0700

    update resume

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,4 +1,4 @@
- $('a[href*="#"]').click(function() {
+$('a[href*="#"]').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
